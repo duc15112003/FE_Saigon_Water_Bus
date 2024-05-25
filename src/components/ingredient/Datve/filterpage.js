7a0d8a08afd1d1449655711cfae2665d46a26af7
Nguyễn Hoàng Vinh <96643414+Hoangvinh261999@users.noreply.github.com>
2024-05-25 20:24:24 +0700

    Trang dat ve ,component ,chon ghe

---
+++ src/components/ingredient/Datve/filterpage.js
@@ -0,0 +1,9 @@
+function FilterPage() {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+
+export default FilterPage;
--- src/components/ingredient/Datve/ghetau.js
+++ src/components/ingredient/Datve/ghetau.js
@@ -1,18 +1,50 @@
 import React, { useState,useEffect } from 'react';
 
+
 const seatLabels = [
-  ["1A", "1B", "1C", "", "1D", "1E", "1F"],
-  ["2A", "2B", "2C", "", "2D", "2E", "2F"],
-  ["3A", "3B", "3C", "", "3D", "3E", "3F"],
-  ["4A", "4B", "4C", "", "4D", "4E", "4F"],
-  ["5A", "5B", "5C", "", "5D", "5E", "5F"],
-  ["6A", "6B", "6C", "", "6D", "6E", "6F"],
-  ["7A", "7B", "7C", "", "7D", "7E", "7F"],
-  ["8A", "8B", "8C", "", "8D", "8E", "8F"],
-  ["9A", "9B", "9C", "", "9D", "9E", "9F"],
-  ["10A", "10B", "10C", "", "10D", "10E", "10F"],
-  ["11A", "11B", "11C", "", "11D", "11E", "11F"],
-  ["12A", "12B", "12C", "", "12D", "12E", "12F"]
+  
+    { id: 1, seat_name: "1A", status: true },
+    { id: 2, seat_name: "1B", status: true },
+    { id: 3, seat_name: "1C", status: true },
+
+    { id: 4, seat_name: "1D", status: false },
+    { id: 5, seat_name: "1E", status: true },
+    { id: 6, seat_name: "1F", status: false },
+    { id: 7, seat_name: "2A", status: true  },
+    { id: 8, seat_name: "2B", status: true },
+    { id: 9, seat_name: "2C", status: true },
+    { id: 10, seat_name: "2D", status: true },
+    { id: 11, seat_name: "2E", status: false },
+    { id: 12, seat_name: "2F", status: false }
+,
+  { id: 13, seat_name: "3A", status: true },
+  { id: 14, seat_name: "3B", status: true  },
+  { id: 15, seat_name: "3C", status: true },
+  { id: 16, seat_name: "3D", status: false },
+  { id: 17, seat_name: "3E", status: true },
+    { id: 18, seat_name: "3F", status: true },
+ { id: 19, seat_name: "3G", status: false },
+  { id: 20, seat_name: "3H", status: true },
+  { id: 21, seat_name: "3I", status: false },
+  { id: 22, seat_name: "3J", status: true },
+  { id: 23, seat_name: "3K", status: false },
+  { id: 24, seat_name: "3L", status: true },
+  { id: 25, seat_name: "4A", status: false },
+  { id: 26, seat_name: "4B", status: true },
+  { id: 27, seat_name: "4C", status: false },
+  { id: 28, seat_name: "4D", status: true },
+  { id: 29, seat_name: "4E", status: false },
+  { id: 30, seat_name: "4F", status: true },
+  { id: 31, seat_name: "4G", status: false },
+  { id: 32, seat_name: "4H", status: true },
+  { id: 33, seat_name: "4I", status: false },
+  { id: 34, seat_name: "4J", status: true },
+  { id: 35, seat_name: "4K", status: false },
+  { id: 36, seat_name: "4L", status: true },
+  { id: 37, seat_name: "5A", status: false },
+  { id: 38, seat_name: "5B", status: true }
+
+
 ];
 
 const SeatingChart = () => {
@@ -32,23 +64,111 @@ const SeatingChart = () => {
   }, [clickedSeats]);
   
   
-  const renderSeat = (seat) => {
-    const isSelected = clickedSeats.includes(seat);
-    const seatColor = isSelected ? "#B8B8B8" : "#B8B8B8"; // Cha
-    return (
-      <div className="seat-item available z-max text-sm" key={seat}>
-        <div className="Seat__SeatContainer-cRMPdR eXOSRu" style={{ color: seatColor, position: 'relative' }}>
-          <div className="Seat__SeatCode-jeBMar bTYNom" style={{ color: seatColor, height: 32, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} onClick={() => handleSeatClick(seat)}>
-            
-            <div>{seat}</div>
+ const renderSeat = (seat) => {
+  const isSelected = clickedSeats.includes(seat);
+  return (
+    <div className=''>
+      <div className={`seat-item available z-max text-sm ${isSelected ? 'selected-seat' : ''} ${seat.status ? '' : 'seat-type-info disabled unavailable'}`} key={seat}>
+        <div className={`relative `}>
+          <div
+            className="flex items-center justify-center h-12 w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={() => seat.status&&handleSeatClick(seat)}
+          >
+<span className={`relative cursor-pointer text-sm font-semibold ${isSelected ? 'text-transparent' : ''}`}>{seat.seat_name}</span>
+
           </div>
+<div className={`${isSelected ? 'seat-type-info selected ' : ''}`} color="#B8B8B8">
+  <svg
+    width={48}
+    height={40}
+    viewBox="0 0 40 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="8.75"
+      y="2.75"
+      width="22.5"
+      height="26.5"
+      rx="2.25"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="10.25"
+      y="11.75"
+      width="14.5"
+      height="5.5"
+      rx="2.25"
+      transform="rotate(90 10.25 11.75)"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="35.25"
+      y="11.75"
+      width="14.5"
+      height="5.5"
+      rx="2.25"
+      transform="rotate(90 35.25 11.75)"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="8.75"
+      y="22.75"
+      width="22.5"
+      height="6.5"
+      rx="2.25"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <path
+      className="icon-selected"
+      d="M20.0002 6.33337C16.3202 6.33337 13.3335 9.32004 13.3335 13C13.3335 16.68 16.3202 19.6667 20.0002 19.6667C23.6802 19.6667 26.6668 16.68 26.6668 13C26.6668 9.32004 23.6802 6.33337 20.0002 6.33337ZM18.6668 16.3334L15.3335 13L16.2735 12.06L18.6668 14.4467L23.7268 9.38671L24.6668 10.3334L18.6668 16.3334Z"
+      fill="transparent"
+    />
+    <path
+      className="icon-disabled"
+      d="M24.96 9.45992L23.54 8.03992L20 11.5899L16.46 8.03992L15.04 9.45992L18.59 12.9999L15.04 16.5399L16.46 17.9599L20 14.4099L23.54 17.9599L24.96 16.5399L21.41 12.9999L24.96 9.45992Z"
+      fill="transparent"
+    />
+  </svg>
+</div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+const chunkSize = 6;
+const seatChunks = [];
+for (let i = 0; i < seatLabels.length; i += chunkSize) {
+  seatChunks.push(seatLabels.slice(i, i + chunkSize));
+}
+
+
+// render seet
+  return (
+<div className='container max-w-xl mx-auto'>
+  <div className="flex flex-col items-center mt-10">
+<div className='flex gap-32 py-4'>
+        <div className="seat-type-info available">
           <svg
             width={40}
             height={32}
             viewBox="0 0 40 32"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="cursor-pointer"
           >
             <rect
               x="8.75"
@@ -57,7 +177,7 @@ const SeatingChart = () => {
               height="26.5"
               rx="2.25"
               fill="#FFFFFF"
-              stroke={seatColor}
+              stroke="#B8B8B8"
               strokeWidth="1.5"
               strokeLinejoin="round"
             />
@@ -69,7 +189,7 @@ const SeatingChart = () => {
               rx="2.25"
               transform="rotate(90 10.25 11.75)"
               fill="#FFFFFF"
-              stroke={seatColor}
+              stroke="#B8B8B8"
               strokeWidth="1.5"
               strokeLinejoin="round"
             />
@@ -81,7 +201,7 @@ const SeatingChart = () => {
               rx="2.25"
               transform="rotate(90 35.25 11.75)"
               fill="#FFFFFF"
-              stroke={seatColor}
+              stroke="#B8B8B8"
               strokeWidth="1.5"
               strokeLinejoin="round"
             />
@@ -92,40 +212,139 @@ const SeatingChart = () => {
               height="6.5"
               rx="2.25"
               fill="#FFFFFF"
-              stroke={seatColor}
+              stroke="#B8B8B8"
               strokeWidth="1.5"
               strokeLinejoin="round"
             />
-            {isSelected ? (
-<svg
-  viewBox="0 0 512 512"
-  fill="#00FF00"
-  height="1.5em"
-  width="1.5em"
->
-
-      <path d="M243.8 339.8c-10.9 10.9-28.7 10.9-39.6 0l-64-64c-10.9-10.9-10.9-28.7 0-39.6 10.9-10.9 28.7-10.9 39.6 0l44.2 44.2 108.2-108.2c10.9-10.9 28.7-10.9 39.6 0 10.9 10.9 10.9 28.7 0 39.6l-128 128zM512 256c0 141.4-114.6 256-256 256S0 397.4 0 256 114.6 0 256 0s256 114.6 256 256zM256 48C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48z" />
-    </svg>
-
-            ) :''}
+            <path
+              className="icon-selected"
+              d="M20.0002 6.33337C16.3202 6.33337 13.3335 9.32004 13.3335 13C13.3335 16.68 16.3202 19.6667 20.0002 19.6667C23.6802 19.6667 26.6668 16.68 26.6668 13C26.6668 9.32004 23.6802 6.33337 20.0002 6.33337ZM18.6668 16.3334L15.3335 13L16.2735 12.06L18.6668 14.4467L23.7268 9.38671L24.6668 10.3334L18.6668 16.3334Z"
+              fill="transparent"
+            />
+            <path
+              className="icon-disabled"
+              d="M24.96 9.45992L23.54 8.03992L20 11.5899L16.46 8.03992L15.04 9.45992L18.59 12.9999L15.04 16.5399L16.46 17.9599L20 14.4099L23.54 17.9599L24.96 16.5399L21.41 12.9999L24.96 9.45992Z"
+              fill="transparent"
+            />
           </svg>
+          <div className="seat-type-info-value">Còn trống</div>
         </div>
-      </div>
-    );
-  };
+        <div className="seat-type-info unavailable">
+
+  <svg
+    width={48}
+    height={40}
+    viewBox="0 0 40 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="8.75"
+      y="2.75"
+      width="22.5"
+      height="26.5"
+      rx="2.25"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="10.25"
+      y="11.75"
+      width="14.5"
+      height="5.5"
+      rx="2.25"
+      transform="rotate(90 10.25 11.75)"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="35.25"
+      y="11.75"
+      width="14.5"
+      height="5.5"
+      rx="2.25"
+      transform="rotate(90 35.25 11.75)"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <rect
+      x="8.75"
+      y="22.75"
+      width="22.5"
+      height="6.5"
+      rx="2.25"
+      fill="#FFFFFF"
+      stroke="#B8B8B8"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <path
+      className="icon-selected"
+      d="M20.0002 6.33337C16.3202 6.33337 13.3335 9.32004 13.3335 13C13.3335 16.68 16.3202 19.6667 20.0002 19.6667C23.6802 19.6667 26.6668 16.68 26.6668 13C26.6668 9.32004 23.6802 6.33337 20.0002 6.33337ZM18.6668 16.3334L15.3335 13L16.2735 12.06L18.6668 14.4467L23.7268 9.38671L24.6668 10.3334L18.6668 16.3334Z"
+      fill="transparent"
+    />
+    <path
+      className="icon-disabled"
+      d="M24.96 9.45992L23.54 8.03992L20 11.5899L16.46 8.03992L15.04 9.45992L18.59 12.9999L15.04 16.5399L16.46 17.9599L20 14.4099L23.54 17.9599L24.96 16.5399L21.41 12.9999L24.96 9.45992Z"
+      fill="transparent"
+    />
+  </svg>
+  <div className="seat-type-info-value">Ghế không bán</div>
+</div>
+
+<div class="seat-type-info selected"><svg width="40" height="32" viewBox="0 0 40 32" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="8.75" y="2.75" width="22.5" height="26.5" rx="2.25" fill="#FFFFFF" stroke="#B8B8B8" stroke-width="1.5" stroke-linejoin="round"></rect><rect x="10.25" y="11.75" width="14.5" height="5.5" rx="2.25" transform="rotate(90 10.25 11.75)" fill="#FFFFFF" stroke="#B8B8B8" stroke-width="1.5" stroke-linejoin="round"></rect><rect x="35.25" y="11.75" width="14.5" height="5.5" rx="2.25" transform="rotate(90 35.25 11.75)" fill="#FFFFFF" stroke="#B8B8B8" stroke-width="1.5" stroke-linejoin="round"></rect><rect x="8.75" y="22.75" width="22.5" height="6.5" rx="2.25" fill="#FFFFFF" stroke="#B8B8B8" stroke-width="1.5" stroke-linejoin="round"></rect><path class="icon-selected" d="M20.0002 6.33337C16.3202 6.33337 13.3335 9.32004 13.3335 13C13.3335 16.68 16.3202 19.6667 20.0002 19.6667C23.6802 19.6667 26.6668 16.68 26.6668 13C26.6668 9.32004 23.6802 6.33337 20.0002 6.33337ZM18.6668 16.3334L15.3335 13L16.2735 12.06L18.6668 14.4467L23.7268 9.38671L24.6668 10.3334L18.6668 16.3334Z" fill="transparent"></path><path class="icon-disabled" d="M24.96 9.45992L23.54 8.03992L20 11.5899L16.46 8.03992L15.04 9.45992L18.59 12.9999L15.04 16.5399L16.46 17.9599L20 14.4099L23.54 17.9599L24.96 16.5399L21.41 12.9999L24.96 9.45992Z" fill="transparent"></path></svg><div class="seat-type-info-value">Đang chọn</div></div>
+</div>
+<div className="flex flex-wrap justify-center mb-2 bg-white">
+<div className="grid grid-cols-7 gap-2 justify-center py-6">
+  {seatLabels.map((seat, index) => (
+    <React.Fragment key={seat.id}>
+      {renderSeat(seat)}
+      {(seat.id ) === 3 + 6 * Math.floor(index / 6) && <div className="col-span-1"></div>}
+    </React.Fragment>
+  ))}
+</div>
 
 
-  return (
-<div className="flex flex-col items-center mt-10">
 
-      {seatLabels.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex justify-center mb-2">
-          {row.map((seat, seatIndex) =>
-            seat ? renderSeat(seat) : <div key={seatIndex} className="w-10 h-10 m-1 text-sm"></div>
-          )}
-        </div>
-      ))}
+
+
+</div>
+<div>
+{clickedSeats === 0 && (
+  <div>
+    <h1>Vui lòng chọn ít nhất 1 chỗ ngồi</h1>
+  </div>
+)}
+
+
+</div>
+
+
+
     </div>
+<div className='container mx-auto flex'>
+  <div className="flex-grow">
+ 
+      <span className='font-bold'>Số ghế:</span>
+  
+    {clickedSeats.map((seat, index) => (
+      <span key={seat.id}>{seat.seat_name}{index !== clickedSeats.length - 1 && ', '}</span>
+    ))}
+  </div>
+  <div>
+    Tổng tiền: {clickedSeats.length * 15000}
+  </div>
+</div>
+
+
+
+</div>
   );
 };
 
