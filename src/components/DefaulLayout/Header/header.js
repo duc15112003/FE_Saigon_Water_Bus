15865b135e4d76ecc36d9b8c962219ce85c8a773
duc15112003 <phamminhduc15112003dilinh@gmail.com>
2024-08-02 18:49:49 +0700

    sua navbar cho menu

--- src/components/DefaulLayout/Header/header.js
+++ src/components/DefaulLayout/Header/header.js
@@ -1,145 +1,115 @@
-
-import { useState } from "react";
+// src/components/Navbar.js
+import React, { useState } from 'react';
+import { Bars3Icon, XMarkIcon, UserIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 import { useAuth } from "../../../AuthContext";
 
+const Navbar = () => {
+    const { isLoggedIn, logout } = useAuth();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const username = localStorage.getItem('us');
+    const [isOpen, setIsOpen] = useState(false);
 
-function Header() {
-  const { isLoggedIn, logout } = useAuth();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-
-            const username = localStorage.getItem('us');
-            // console.log(sessionStorage.getItem('us'))
     return (
-        <div className="Header  bg-white w-full  fixed " style={{zIndex:'9999'}}>
-
-      <div className={`fixed w-3/4 h-full top-0 right-0 bg-white shadow-lg rounded-md transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-40`}>
-        <button onClick={toggleMenu} className="absolute right-4 top-4 text-gray-600 hover:text-gray-800">
-          Đóng
-        </button>
-        <ul className="w-full px-4 py-8 text-base font-semibold space-y-4 bg-gray-50 rounded-lg">
-          <li>
-            <a className="block hover:text-gray-600" href="/">Trang Chủ</a>
-          </li>
-          <li>
-            <a className="block hover:text-gray-600" href="/ben-tau">Bến Tàu</a>
-          </li>
-          <li>
-            <a className="block hover:text-gray-600" href="/lich">Lịch khởi hành</a>
-          </li>
-          <li>
-            <a className="block hover:text-gray-600" href="/dat-ve">Đặt Vé</a>
-          </li>
-          <li>
-            <a className="block hover:text-gray-600" href="/contact">Liên Hệ</a>
-          </li>
-          {isLoggedIn ? (
-            <div className="flex items-center space-x-4">
-              <a className="flex items-center hover:text-gray-600" href="/profile">
-                <img src="/img/icon/user.png" alt="User Icon" className="h-6 w-6 mr-2"/>
-                <span>{username}</span>
-              </a>
-              <button
-                className="flex items-center hover:text-gray-600"
-                onClick={logout}
-              >
-                <img src="/img/icon/logout.png" alt="Logout Icon" className="h-6 w-6 mr-2"/>
-                <span>Đăng xuất</span>
-              </button>
-            </div>
-          ) : (
-            <a className="flex items-center hover:text-gray-600" href="/login">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              Đăng nhập
-            </a>
-          )}
-        </ul>
-      </div>
-
-            <section className="relative mx-auto">
-                <nav className="flex justify-between bg-slate-50 text-black w-full shadow-lg">
-                    <div className="px-5 xl:px-12 py-2 flex w-full items-center">
-                        <div className="w-2/12 justify-center flex">
-                            <a className="text-3xl font-bold font-heading" href="/">
-                            <img className="h-12 w-15" alt='' src="/img/logo_footer.png" />
-                        </a>
-                        </div>
-                        
-                        <div className="w-8/12 ">
-                                <ul className="hidden md:flex px-1 mx-auto xl:text-base text-sm lg:gap-2 font-semibold font-heading space-x-12 justify-center">
-                                    <li>
-                                        <a className="hover:text-gray-200" href="/">Trang Chủ</a>
-                                    </li>
-                                    <li>
-                                        <a className="hover:text-gray-200" href="/ben-tau">Bến Tàu</a>
-                                    </li>
-
-                                    <li>
-                                        <a className="hover:text-gray-200" href="/lich">Lịch khởi hành</a>
-                                    </li>
-
-                                    <li>
-                                        <a className="hover:text-gray-200" href="/dat-ve">Đặt Vé</a>
-                                    </li>
-                                    <li>
-                                        <a className="hover:text-gray-200" href="/contact">Liên Hệ</a>
-                                    </li>
-                                    {isLoggedIn && (
-                                        <li>
-                                            <a className="hover:text-gray-200" href="/lich-su-dat-ve">Lịch sử đặt vé</a>
-                                        </li>
-                                    )}
-                                </ul>
-                        </div>
-                        
-                        <div className="hidden xl:flex space-x-5 items-center w-2/12 justify-end">
-
-
-                    {isLoggedIn ? (
-        <div className="flex items-center">
-          <a className="flex items-center hover:text-gray-200" href="/profile">
-            <img src="/img/icon/user.png" alt="" className="px-1"/>
-           <label className="font-bold  font-bold text-sm">{username}</label>
-          </a>
-          <button
-            className="flex items-center hover:text-gray-200 ml-4"
-            onClick={logout}
-          >
-            
-                   <img src="/img/icon/logout.png" alt="" className="px-1"/>
-                              <label className=" font-bold text-sm">Đăng xuất</label>
-
-
-          </button>
-        </div>
-      ) : (
-        <a className="flex items-center hover:text-gray-200" href="/login">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-          Đăng nhập
-        </a>
-      )}
-         </div>
-                    </div>
-   
-                    <button onClick={toggleMenu} className="navbar-burger xl:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
+        <nav className="bg-white p-4 navbar-responsive relative z-50">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-black text-lg font-bold flex">
+                  <div className='mt-2'>
+                    <img className="h-10 w-15" alt='' src="/img/logo_footer.png" />
+                  </div>
+                  <p className='mt-2 ml-1'>Saigon Waterbus</p>
+                </div>
+                <div className="block lg:hidden">
+                    <button onClick={toggleMenu} className="text-gray-300 hover:text-white focus:outline-none">
+                        {isOpen ? <XMarkIcon className="h-6 w-6" /> : <Bars3Icon className="h-6 w-6" />}
                     </button>
-                  
-
-                </nav>
-            </section>
-        </div>
+                </div>
+                <div className={`lg:flex lg:flex-row lg:items-center ${isOpen ? 'flex' : 'hidden'} flex-col lg:static absolute top-16 left-0 lg:top-0 bg-white w-full lg:w-auto border-t lg:border-none`}>
+                    <div className="flex flex-col items-start py-4 bg-white w-full lg:hidden">
+                        {isLoggedIn ? (
+                            <div className="ml-4">
+                                <img src="https://th.bing.com/th/id/OIP.HHVUf3TYqncgpJXyCMmxyAHaHa?pid=ImgDet&w=185&h=185&c=7" alt="User" className="rounded-full w-20 h-20 mb-2" />
+                                <h3 className="text-black text-lg">{username}</h3>
+                                <div className="flex">
+                                    <a className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none flex w-auto" href='/profile'>
+                                    <div className='mt-1'>
+                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                        </svg>
+                                      </div>
+                                      <p className='ml-2'> Thông tin tài khoản</p>
+                                    </a>
+                                    <button onClick={logout} className="mt-2 ml-2 px-4 py-2 bg-blue-500 text-white flex rounded hover:bg-blue-600 focus:outline-none w-auto">
+                                      <div className='mt-1'>
+                                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                          <path fill-rule="evenodd" d="M14 4.75A2.75 2.75 0 0 0 11.25 2h-3A2.75 2.75 0 0 0 5.5 4.75v.5a.75.75 0 0 0 1.5 0v-.5c0-.69.56-1.25 1.25-1.25h3c.69 0 1.25.56 1.25 1.25v6.5c0 .69-.56 1.25-1.25 1.25h-3c-.69 0-1.25-.56-1.25-1.25v-.5a.75.75 0 0 0-1.5 0v.5A2.75 2.75 0 0 0 8.25 14h3A2.75 2.75 0 0 0 14 11.25v-6.5Zm-9.47.47a.75.75 0 0 0-1.06 0L1.22 7.47a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06l-.97-.97h7.19a.75.75 0 0 0 0-1.5H3.56l.97-.97a.75.75 0 0 0 0-1.06Z" clip-rule="evenodd" />
+                                        </svg>
+                                      </div>
+                                    <p className='ml-2'>Đăng xuất</p>
+                                    </button>
+                                </div>
+                            </div>
+                        ) : (
+                            <a className="flex items-center hover:text-gray-600 ml-4" href="/login">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                Đăng nhập
+                            </a>
+                        )}
+                    </div>
+                    <ul className="flex flex-col lg:flex-row lg:items-center lg:ml-auto w-full lg:w-auto lg:py-0 py-4">
+                        <li className="lg:ml-4 ml-4">
+                            <a href="/" className="block py-2 px-4 text-black hover:text-red">Trang Chủ</a>
+                        </li>
+                        <li className="lg:ml-4 ml-4">
+                            <a href="/ben-tau" className="block py-2 px-4 text-black hover:text-red">Bến tàu</a>
+                        </li>
+                        <li className="lg:ml-4 ml-4">
+                            <a href="/lich" className="block py-2 px-4 text-black hover:text-red">Lịch khởi hành</a>
+                        </li>
+                        <li className="lg:ml-4 ml-4">
+                            <a href="/dat-ve" className="block py-2 px-4 text-black hover:text-red">Đặt vé</a>
+                        </li>
+                        <li className="lg:ml-4 ml-4">
+                            <a href="/contact" className="block py-2 px-4 text-black hover:text-red">Liên hệ</a>
+                        </li>
+                        {isLoggedIn && (
+                            <li className="lg:ml-4 ml-4">
+                                <a className="block py-2 px-4 text-black hover:text-red" href="/lich-su-dat-ve">Lịch sử đặt vé</a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                {isLoggedIn ? (
+                    <div className="hidden lg:flex items-center space-x-4 lg:space-x-6">
+                        <div className="text-black flex items-center">
+                            <UserIcon className="h-6 w-6 inline-block mr-2 bold" />
+                            <span>{username}</span>
+                        </div>
+                        <button onClick={logout} className="text-black hover:text-green focus:outline-none flex ">
+                        <div className='mt-1'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                          <path fill-rule="evenodd" d="M14 4.75A2.75 2.75 0 0 0 11.25 2h-3A2.75 2.75 0 0 0 5.5 4.75v.5a.75.75 0 0 0 1.5 0v-.5c0-.69.56-1.25 1.25-1.25h3c.69 0 1.25.56 1.25 1.25v6.5c0 .69-.56 1.25-1.25 1.25h-3c-.69 0-1.25-.56-1.25-1.25v-.5a.75.75 0 0 0-1.5 0v.5A2.75 2.75 0 0 0 8.25 14h3A2.75 2.75 0 0 0 14 11.25v-6.5Zm-9.47.47a.75.75 0 0 0-1.06 0L1.22 7.47a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06l-.97-.97h7.19a.75.75 0 0 0 0-1.5H3.56l.97-.97a.75.75 0 0 0 0-1.06Z" clip-rule="evenodd" />
+                        </svg> 
+                        </div>
+                       <p className='ml-1'>Đăng xuất</p>
+                        </button>
+                    </div>
+                ) : (
+                    <a className="hidden lg:flex items-center hover:text-gray-200" href="/login">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        Đăng nhập
+                    </a>
+                )}
+            </div>
+        </nav>
     );
-}
+};
 
-export default Header;
+export default Navbar;
